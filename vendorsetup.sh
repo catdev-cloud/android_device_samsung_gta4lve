f38c9be0fdba6980b11cb8713cc33167368fd72e
catdev-cloud <catdev3github@gmail.com>
2025-12-01 07:16:43 +0200

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_gta4lve-user
-add_lunch_combo omni_gta4lve-userdebug
-add_lunch_combo omni_gta4lve-eng
+add_lunch_combo twrp_gta4lve-user
+add_lunch_combo twrp_gta4lve-userdebug
+add_lunch_combo twrp_gta4lve-eng
